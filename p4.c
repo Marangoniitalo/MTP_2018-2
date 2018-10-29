7eb79a86312b823b1c5e05441629e13fc5a519e8
Ítalo Marangoni de Souza <42525080+Marangoniitalo@users.noreply.github.com>
2018-10-29 19:49:13 -0300

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,36 @@
+//Ítalo Marangoni de Souza
+
+//11811EAU014
+
+//MTP_2018-2
+
+#include <stdio.h>
+
+int funcao(int m, int n)
+{
+	int i;
+	if(m == 0)
+	{
+		return n+1;
+    }
+    else if(m >0 && n == 0)
+    {
+    	return funcao(m-1, 1);
+	}
+	else if(m > 0 && n > 0)
+	{
+		return funcao(m-1, funcao(m, n-1));
+	}	
+}
+int main()
+{
+	int m, n;
+	printf("Digite o valor de m: ");
+	scanf("%d", &m);
+	getchar();
+	printf("Digite o valor de n: ");
+	scanf("%d", &n);
+	getchar();
+    printf("\n\nResultado da funcao: %d", funcao(m,n));
+    return 0;
+}
